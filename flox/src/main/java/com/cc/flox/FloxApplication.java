85baffbebcca94c16a947a1d1ef8511601c31dfd
cc13446 <1344613920@qq.com>
2024-03-20 21:42:25 +0800

    init

---
+++ flox/src/main/java/com/cc/flox/FloxApplication.java
@@ -0,0 +1,13 @@
+package com.cc.flox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloxApplication.class, args);
+	}
+
+}
