4c6da249ca5e449e039beed42f0ade0729ce9f51
cc13446 <1344613920@qq.com>
2024-03-30 20:03:38 +0800

    注入 http handler

--- flox/src/main/java/com/cc/flox/web/DispatchHttpHandler.java
+++ flox/src/main/java/com/cc/flox/web/DispatchHttpHandler.java
@@ -3,6 +3,7 @@ package com.cc.flox.web;
 import org.springframework.http.server.reactive.HttpHandler;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 import reactor.util.annotation.NonNull;
 
@@ -10,11 +11,11 @@ import reactor.util.annotation.NonNull;
  * @author cc
  * @date 2024/3/30
  */
+@Component()
 public class DispatchHttpHandler implements HttpHandler {
 
     @Override
     public @NonNull Mono<Void> handle(@NonNull ServerHttpRequest request, @NonNull ServerHttpResponse response) {
-
         System.out.println("aa");
         return Mono.empty();
     }
