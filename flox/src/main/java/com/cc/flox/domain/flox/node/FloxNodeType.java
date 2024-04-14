eb420ad2e490ec7e65288f2a03142097fdc03300
cc13446 <1344613920@qq.com>
2024-04-14 21:00:31 +0800

    flox node

---
+++ flox/src/main/java/com/cc/flox/domain/flox/node/FloxNodeType.java
@@ -0,0 +1,37 @@
+package com.cc.flox.domain.flox.node;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 节点类型
+ *
+ * @author cc
+ * @date 2024/4/14
+ */
+@AllArgsConstructor
+@Getter
+public enum FloxNodeType {
+
+    EXTRACT("extract"),
+
+    REQUEST_EXTRACT("requestExtract"),
+
+    LOADER("loader"),
+
+    RESPONSE_LOADER("responseLoader"),
+
+    DATA_SOURCE_LOADER("dataSourceLoader"),
+
+    TRANSFORMER("transformer"),
+
+    BI_TRANSFORMER("biTransformer"),
+
+    TRI_TRANSFORMER("triTransformer");
+
+    /**
+     * code
+     */
+    private final String code;
+
+}
