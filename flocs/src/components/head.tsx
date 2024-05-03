46666509858885a3207107002e6850ee0214cc14
cc13446 <1344613920@qq.com>
2024-05-03 13:43:42 +0800

    select-none

--- flocs/src/components/head.tsx
+++ flocs/src/components/head.tsx
@@ -5,14 +5,14 @@ import Logo from '../../public/logo.svg'
 
 const App: React.FC = () => {
     return (
-        <div className="w-full h-16 border-2 flex flex-row justify-between items-center">
+        <div className="w-full h-16 border-2 flex flex-row justify-between items-center select-none">
 
-            <div className="w-40 h-full pl-3 flex flex-row justify-between items-center">
+            <div className="w-40 h-full pl-3 flex flex-row justify-between items-center select-none">
                 <Image src={Logo} alt='logo' className='h-12' />
                 <p className='font-bold text-3xl text-logo-color'>FLOX</p>
             </div>
-            <div className='w-20 h-full flex flex-row justify-between items-center'>
-            <p className='font-bold'><a href='https://github.com/cc13446/flox'>About</a></p>
+            <div className='w-20 h-full flex flex-row justify-between items-center select-none'>
+            <p className='font-bold select-none'><a href='https://github.com/cc13446/flox'>About</a></p>
             </div>
         </div>
 
