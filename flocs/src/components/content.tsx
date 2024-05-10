60742e1b2bca56762d0d0759510873ae4f6cd7d8
cc13446 <1344613920@qq.com>
2024-05-10 22:52:29 +0800

    页面框架

--- flocs/src/components/content.tsx
+++ flocs/src/components/content.tsx
@@ -1,4 +1,12 @@
 import React from 'react';
+import Head from './content/head'
+import Connect from './content/connect'
+import Action from './content/action'
+import DataType from './content/dataType'
+import Node from './content/node'
+import SubFlox from './content/subFlox'
+import Flox from './content/flox'
+import Endpoint from './content/endpoint'
 
 
 interface Props {
@@ -6,9 +14,33 @@ interface Props {
 }
 
 const App: React.FC<Props> = (props: Props) => {
+
+    const renderComponent = () => {
+        switch (props.activeContent) {
+            case 'head':
+                return <Head />;
+            case 'connect':
+                return <Connect />;
+            case 'action':
+                return <Action />;
+            case 'dataType':
+                return <DataType />;
+            case 'node':
+                return <Node />;
+            case 'subFlox':
+                return <SubFlox />;
+            case 'flox':
+                return <Flox />;
+            case 'endpoint':
+                return <Endpoint />;
+            default:
+                return null;
+        }
+    };
+
     return (
-        <div className=''>
-            <p>{props.activeContent}</p>
+        <div className='p-5'>
+            {renderComponent()}
         </div>
     );
 };
