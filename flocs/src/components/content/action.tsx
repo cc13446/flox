b5e3438e4dab55542d3fb33775cfe63de9043392
cc13446 <1344613920@qq.com>
2024-05-12 20:00:17 +0800

    action mount

--- flocs/src/components/content/action.tsx
+++ flocs/src/components/content/action.tsx
@@ -1,6 +1,18 @@
-import React from 'react';
+'use client'
+import React, { useEffect } from 'react';
+
+import Fetch, { IRequestParams, BodyType } from '../../lib/backend';
 
 const App: React.FC = () => {
+
+    useEffect(() => {
+        console.log('Action has mounted.');
+        return () => {
+            console.log('Action will unmount.');
+        };
+    }, []);
+
+
     return (
         <>
             <h1>Action</h1>
---
+++ flocs/src/lib/backend.ts
@@ -0,0 +1,50 @@
+import axios, { AxiosHeaders, AxiosRequestConfig, AxiosResponse, Method } from 'axios';
+
+const backUrl = 'http://127.0.0.1:8080'
+
+const api = axios.create({
+    baseURL: backUrl
+});
+
+export interface IRequestParams {
+    path: string,
+    method: Method,
+    bodyType: BodyType,
+    data: any,
+}
+
+export interface IResponseData {
+    code: number,
+    message: string,
+    data: any,
+}
+
+export enum BodyType {
+    FORM = 'application/x-www-form-urlencoded',
+    JSON = 'application/json'
+}
+
+export default async (params: IRequestParams): Promise<any> => {
+    try {
+        const headers = new AxiosHeaders();
+        headers.set('Content-Type', params.bodyType)
+        const config: AxiosRequestConfig<any> = {
+            url: params.path,
+            method: params.method,
+            headers: headers,
+            data: params.data
+        }
+        const response: AxiosResponse<IResponseData> = await api.request(config);
+        const data = response.data;
+        if (data.code !== 200) {
+            throw new Error(`Request failed with server error: ${data.message}`);
+        }
+        return data.data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            throw new Error(`Request failed with status code: ${error.response?.status}`);
+        } else {
+            throw error;
+        }
+    }
+};
